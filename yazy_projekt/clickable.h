c08da38aed6df88c91a68e0c5956c3604ed996ac
ErikSawander0 <eriksawander@gmail.com>
2025-01-14 14:24:32 +0100

    added images

--- yazy_projekt/clickable.h
+++ yazy_projekt/clickable.h
@@ -10,6 +10,7 @@ friend class Game;
 friend class click_engine;
 protected:
     Drawable(Rectangle rect, int id, Game* owner);
+    Drawable(Rectangle rect, int id, Game* owner, Texture2D texture);
     virtual void update() {} // Default update does nothing
     virtual void draw(); 
     int get_id();
@@ -21,6 +22,8 @@ protected:
     Rectangle rect;
     Color color = {0, 0, 0, 255};
     Game* owner;
+    Texture2D texture;
+    bool has_texture;
 };
 
 class Clickable : public Drawable {
@@ -28,14 +31,17 @@ friend class Game;
 friend class click_engine;
 protected:
     Clickable(Rectangle rect, std::function<void()> onClick, Game* owner, int id);  
+    Clickable(Rectangle rect, std::function<void()> onClick, Game* owner, int id, Texture2D texture);  
     void remove();  
     void update() override;
     std::function<void()> onClick;
 };
 
-class click_engine{
+class click_engine {
 public: 
     static void make_clickable(Rectangle rect, std::function<void()> onClick, Game* owner, int id);
+    static void make_clickable(Rectangle rect, std::function<void()> onClick, Game* owner, int id, Texture2D texture);
     static void make_drawable(Rectangle rect, int id, Game* game);
+    static void make_drawable(Rectangle rect, int id, Game* game, Texture2D texture);
 };
 #endif // !CLICKABLE_H
