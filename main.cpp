ef69c8a963427449ad6050aeaf380ecda990f560
Ellev <Ellevondan@gmail.com>
2025-01-15 21:06:31 +0100

    fixed reroll buttons

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ class YazyGame {
 
     std::vector<bool> reroll_flags = {true, true, true, true, true};
 
-    Rectangle roll_all_button = {400, 200, 50, 20};
-    Rectangle roll_some_button = {200, 200, 50, 20};
+    Rectangle roll_all_button = {350, 450, 50, 20};
+    Rectangle roll_some_button = {250, 450, 50, 20};
 
     Rectangle dice1_button = {100, 500, 50, 50};
     Rectangle dice2_button = {200, 500, 50, 50};
@@ -34,6 +34,9 @@ class YazyGame {
     const int dice4_button_id = 5;
     const int dice5_button_id = 6;
 
+    const int roll_all_text_id = 7;
+    const int roll_some_text_id = 8;
+
     Texture2D dice1_texture = LoadTexture("C:/Users/Ellev/ClionProjects/programmeringsmetodik-projekt/Data/dice1.png");
     Texture2D dice2_texture = LoadTexture("C:/Users/Ellev/ClionProjects/programmeringsmetodik-projekt/Data/dice2.png");
     Texture2D dice3_texture = LoadTexture("C:/Users/Ellev/ClionProjects/programmeringsmetodik-projekt/Data/dice3.png");
@@ -45,14 +48,15 @@ class YazyGame {
     public:
     YazyGame(Game& game) : game(game) {
 
-        click_engine::make_clickable(roll_all_button, [this]() { logic.rollAllDice(); }, game, roll_all_button_id);
-        click_engine::make_clickable(roll_some_button, [this]() { logic.rerollDice(reroll_flags); }, game, roll_some_button_id);
+        click_engine::make_clickable(roll_all_button, [this]() { logic.rollAllDice(); }, game, roll_all_button_id, "Roll all");
+        click_engine::make_clickable(roll_some_button, [this]() { logic.rerollDice(reroll_flags); }, game, roll_some_button_id, "Reroll");
 
         click_engine::make_clickable(dice1_button, [this]() {reroll_flags[0] = !reroll_flags[0]; std::cout<< "Dice 1 reroll = " << reroll_flags[0] << std::endl;}, game, dice1_button_id, dice1_texture);
         click_engine::make_clickable(dice2_button, [this]() {reroll_flags[1] = !reroll_flags[1]; std::cout<< "Dice 2 reroll = " << reroll_flags[1] << std::endl;}, game, dice2_button_id, dice2_texture);
         click_engine::make_clickable(dice3_button, [this]() {reroll_flags[2] = !reroll_flags[2]; std::cout<< "Dice 3 reroll = " << reroll_flags[2] << std::endl;}, game, dice3_button_id, dice3_texture);
         click_engine::make_clickable(dice4_button, [this]() {reroll_flags[3] = !reroll_flags[3]; std::cout<< "Dice 4 reroll = " << reroll_flags[3] << std::endl;}, game, dice4_button_id, dice4_texture);
         click_engine::make_clickable(dice5_button, [this]() {reroll_flags[4] = !reroll_flags[4]; std::cout<< "Dice 5 reroll = " << reroll_flags[4] << std::endl;}, game, dice5_button_id, dice5_texture);
+
     };
 };
 
