9534cf58a3160c38a0a5e05a61e74d26e8adfe45
Ellev <Ellevondan@gmail.com>
2025-01-16 00:07:28 +0100

    declared score saving functions and persistent scores

--- yazyfunct.h
+++ yazyfunct.h
@@ -9,6 +9,21 @@
 
 class yazyfunct
 {
+private:
+    int ones_score = 0;
+    int twos_score = 0;
+    int threes_score = 0;
+    int fours_score = 0;
+    int fives_score = 0;
+    int sixes_score = 0;
+    int three_of_a_kind_score = 0;
+    int four_of_a_kind_score = 0;
+    int full_house_score = 0;
+    int small_straight_score = 0;
+    int large_straight_score = 0;
+    int yazy_score = 0;
+
+
 public:
     int dice[5];
     bool categoryUsed[13];
@@ -18,6 +33,20 @@ public:
     void rollAllDice();
     void rerollDice(std::vector<bool> reroll_flags);
     void printDice() const;
+
+    // save score functions very proffesional
+    void saveOnesScore();
+    void saveTwosScore();
+    void saveThreesScore();
+    void saveFoursScore();
+    void saveFivesScore();
+    void saveSixesScore();
+    void saveThreeOfAKindScore();
+    void saveFourOfAKindScore();
+    void saveFullHouseScore();
+    void saveSmallStraightScore();
+    void saveLargeStraightScore();
+    void saveYazyScore();
 
     //rules
     int same_number(int match, const int dice[5]);
